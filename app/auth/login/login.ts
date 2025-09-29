bcae2b5092f7a3494bb2270a9255e96b00e2b1d2
HugoCV <huchav24@gmail.com>
2025-09-29 16:26:02 -0600

    added me

---
+++ app/auth/login/login.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { FormError } from '@/app/common/form-error.interface';
+import { API_URL } from '@/app/constants/api';
+import { getErrorMessage } from '@/app/utils/errors';
+import { jwtDecode } from 'jwt-decode';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export default async function login(_prevState: FormError, formData: FormData) {
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(Object.fromEntries(formData)),
+  });
+  const parsedRes = await res.json();
+  if (!res.ok) {
+    return { error: getErrorMessage(parsedRes) };
+  }
+  await setAuthCookie(res);
+  redirect('/');
+}
+
+
+interface JwtPayload {
+  exp: number;
+}
+
+const setAuthCookie = async (response: Response) => {
+  const setCookieHeader = response.headers.get("Set-Cookie");
+  if (setCookieHeader) {
+    const token = setCookieHeader.split(";")[0].split("=")[1];
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    (await cookies()).set({
+        name: "Authentication",
+        value: token,
+        httpOnly: true, 
+        expires: new Date(decodedToken.exp * 1000)
+    });
+  }
+}
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { Button, Link, Stack, TextField } from "@mui/material";
+import NextLink from "next/link";
+
+import { useActionState } from "react";
+import login from "./login";
+
 export default function Login(){
-    return <></>
+    const [state, formAction] = useActionState(login, { error: ""})
+
+    return (<form action={formAction} className="flex flex-col items-center justify-center min-h-screen gap-4">
+        <Stack spacing={2} className="w-full max-w-sx">
+            <TextField name="email" type="email" placeholder="Email" required helperText={state.error} error={!!state.error}/>
+            <TextField name="password" type="password" placeholder="Password" required  helperText={state.error} error={!!state.error} />
+            <Button type="submit" variant="contained">Login</Button>
+            <Link component={NextLink} href="/" className="self-center">
+                Signup
+            </Link>
+        </Stack>
+    </form>);
 }
